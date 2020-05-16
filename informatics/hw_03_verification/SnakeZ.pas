9f017e9c3de2c0513d294cf93c570f1c58abb362
kirillkochnov <65298618+KirillKochnov@users.noreply.github.com>
2020-05-16 20:59:02 +0300

    Add files via upload

---
+++ informatics/hw_03_verification/SnakeZ.pas
@@ -0,0 +1,39 @@
+program SnakeZ;
+
+var
+  N,i,L,K: Integer;
+  A,B,C: Real;
+  T: array of Real;
+
+begin
+  Write('Enter N- ');
+  ReadLn(N);
+  Write('Enter A- ');
+  ReadLn(A);
+  Write('Enter B- ');
+  ReadLn(B);
+  If (A>=B)then
+  begin
+    Write('Oshibka vvoda');
+  end;
+  SetLength(T,N);
+  for i:=0 to N-1 do
+  begin
+    ReadLn(C);
+    T[i]:=C;
+  end;
+  K:=0;
+  for i:=0 to N-1 do
+  begin
+    if not ((T[i]>=A) and (B>T[i])) then
+    begin
+       L:=high(T);
+       T[K]:=T[L];
+       setLength(T,L);
+       K:=K-1;
+    end
+    else K:=K+1;
+  end;
+  ReadLn
+end.
+
