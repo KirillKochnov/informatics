709543fe95f699b7b529d694211ce24ab86a0573
kirillkochnov <65298618+KirillKochnov@users.noreply.github.com>
2020-05-23 18:24:36 +0300

    Add files via upload

--- informatics/hw_03_verification/backup/SnakeZ.pas
+++ informatics/hw_03_verification/backup/SnakeZ.pas
@@ -1,9 +1,9 @@
 program SnakeZ;
 
 var
-  N,i,L,K: Integer;
-  A,B,C: Real;
-  T: array of Real;
+  N,i,K: Integer;
+  A,B,C: Integer;
+  T: array of Integer;
 
 begin
   Write('Enter N- ');
@@ -14,7 +14,9 @@ begin
   ReadLn(B);
   If (A>=B)then
   begin
-    Write('Oshibka vvoda');
+    WriteLn('Oshibka vvoda');
+    ReadLn;
+    Exit();
   end;
   SetLength(T,N);
   for i:=0 to N-1 do
@@ -25,19 +27,21 @@ begin
   K:=0;
   for i:=0 to N-1 do
   begin
-    if not ((T[i]>=A) and (B>T[i])) then
+    if ((T[i]>=A) and (B>T[i])) then
     begin
-       L:=high(T);
-       T[K]:=T[L];
-       setLength(T,L);
-       K:=K-1;
-    end
-    else K:=K+1;
+       T[K]:=T[i];
+       K:=K+1;
+    end;
   end;
-  for i:=0 to high(T)-1 do
+  SetLength(T,K);
+  WriteLn('');
+  WriteLn('Proverka:');
+  WriteLn('');
+  WriteLn('dlina massiva- ',Length(T));
+  for i:=0 to K-1 do
   begin
-    WriteLn(Trunc(T[i]));
+    WriteLn(T[i]);
   end;
-  ReadLn
+  ReadLn;
 end.
 
